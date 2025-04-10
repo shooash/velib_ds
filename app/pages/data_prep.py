af1559973aed67283557c7632cb4db0419937537
shooash <11447353+shooash@users.noreply.github.com>
2025-04-10 11:49:37 +0200

    Graph stations par heure dataset d'origine

--- app/pages/data_prep.py
+++ app/pages/data_prep.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
+def show_date_hour_station_counts():
+    datehour_df = pd.read_hdf('app/data/datehour_stations.h5')
+    return px.line(datehour_df, 'datehour', 'station', labels={'datehour' : 'Date-heure', 'station' : '# de stations'}, title="Nombre de stations connues par heure (données d'origine)")
 
 def show():
-    pass
+    st.plotly_chart(show_date_hour_station_counts())
